2ba176fa084d91ed5cb95c5418ed1038298a6961
Rishabh Jain <mailrishabh1403@gmail.com>
2019-02-15 22:50:48 +0530

    use generic rooms for client connections

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
+app.use(cors({
+  origin: 'http://localhost:3000/',
+  credentials: true,
+}));
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
@@ -8,30 +13,13 @@ const io = require('socket.io').listen(server);
 const sockets = [];
 
 io.on('connection', (socket) => {
-  sockets.push(socket);
-  console.log(`a new client connection ${socket.id}`); //eslint-disable-line
-  if (sockets.length === 2) {
-    io.emit('ready');
-    const socket1 = sockets[0];
-    const socket2 = sockets[1];
-
-    socket1.on('keydown', (...args) => {
-      socket2.emit('keydown', ...args);
-    });
-
-    socket1.on('keyup', (...args) => {
-      socket2.emit('keyup', ...args);
-    });
-
-    socket2.on('keydown', (...args) => {
-      socket1.emit('keydown', ...args);
-    });
-
-    socket2.on('keyup', (...args) => {
-      socket1.emit('keyup', ...args);
-    });
-  }
+  socket.emit('connected',"Hey");
+  socket.on("joinRoom", (data)=>{
+    console.log(data);
+    socket.join(data);
+    io.to(data).emit("Joined room", io.sockets.adapter.rooms[data]);
+  });
 });
 
 
-app.listen('8000', () => console.log('Server listening on port 8000')); // eslint-disable-line
+server.listen('8000', () => console.log('Server listening on port 8000')); // eslint-disable-line
