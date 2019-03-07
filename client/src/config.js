ffdad1ab9b13ce2f8505fe80bf0a55b7e31b63d0
Rishabh Jain <mailrishabh1403@gmail.com>
2019-03-08 04:59:51 +0530

    change config

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const apiBase = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'http://35.196.90.5';
+const apiBase = process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : 'http://35.196.90.5:8000';
 
 export default apiBase;
